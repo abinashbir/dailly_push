b3b2aec6b3cb072907c22642f04c41cc338ea3cf
abinashbir <abinashbir13112@gmail.com>
2025-12-18 19:55:46 +0530

    updated

---
+++ inpuut.java
@@ -0,0 +1,11 @@
+
+import java.util.Scanner;
+
+public class inpuut {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your rollno : ");
+        int rollno = input.nextInt();
+        System.out.println("Your rollno is " + rollno);
+    }
+}
